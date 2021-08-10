a32b7a925ce01311ffe2fed623bcbce722de70a4
Esther YU <togom11@g.skku.edu>
2021-08-10 20:30:01 +0900

    Create moonrace.rb

---
+++ moonrace.rb
@@ -0,0 +1,43 @@
+#whats your name? ask the user for their name
+#get user input for name
+# randomly assign a name to the computer
+
+def roll_dice
+    # a method to roll dice
+
+    # get the user input for the name
+    # (randomly) assign a name to the computer ('computer')
+    #compuer_location = 0
+
+    #start the loop
+    #loop until user_location >= 10 || computer_location >= 10
+        # roll the dice for the user
+        # get input by incrementing(+=) user location with the dice roll
+
+        # roll the dice for the user
+        # increment(+=) user location with the dice roll
+
+        #when do we start?
+        #randomly choose 1-10 or whatever
+       if user_location > computer_location
+           puts 'you win!'
+
+
+        sleep(2 / number.to_f)
+
+        #compare the user_location to the computer_location
+        #whoever is larger, wins
+        #if the user is bigger,
+        #tell the user they won
+        #otherwise,
+        #tell the user they lost
+        puts "ich vermisse mein mann sehr sehr grop."
+
+
+        #roll the dice for the computer
+        #increment (+=) the computer_location with the dice roll
+        #tell the result of the dice roll && new position
+
+    #until
+    #specify moon
+    #end the loop
