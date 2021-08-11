44b4ceab581dc418bcbea2cbd92b727a27d91387
Esther YU <togom11@g.skku.edu>
2021-08-11 11:46:34 +0900

    Create ruby_intro.rb

---
+++ ruby_intro.rb
@@ -0,0 +1,47 @@
+# print "What's your first name? "
+# first_name = gets.chomp
+
+# print "What's your last name? "
+# last_name = gets.chomp
+
+# print "What city are you from? "
+# city = gets.chomp
+
+# print "What state or province are you from? "
+# state = gets.chomp
+
+# first_name = "Kevin"
+# puts "Your name is #{first_name}!"
+
+# print "This is my question?"
+# # answer
+# # answer2 
+# first_name = gets.chomp.capitalize
+# last_name = answer.capitalize
+# city.captalize!
+# state.upcase!
+
+print "What’s your first name? "
+first_name = gets.chomp
+first_name = "esther".capitalize
+first_name.capitalize!
+
+print "What’s your last name? "
+last_name = gets.chomp
+last_name = "Sama".capitalize
+last_name.capitalize!
+
+print "What city are you from? "
+city = gets.chomp
+city = "Zurich".capitalize
+city.capitalize!
+
+print "What state or province are you from? "
+state = gets.chomp
+state = "CH".upcase
+state.upcase!
+
+puts 'Your name is #{first_name} #{last_name} and you’re from #{city}, #{state}!'
+
+
+
